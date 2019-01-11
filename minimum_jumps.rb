358092c980835540f830a3a4e324a901026d4e09
Alberto Vallejo <javr1088@gmail.com>
2019-01-11 17:20:25 -0600

    minimun_jumps: add new scenario

--- minimum_jumps.rb
+++ minimum_jumps.rb
@@ -9,6 +9,8 @@
 #   Ed: Hal
 #   Carl: Hal
 #   Hal: Mark
+#   Mark: Bob, Alice, John
+#   John: Laura
 
 # u1 = Alice
 # u1.friends = [Bod, Carl, Dan]
@@ -19,15 +21,17 @@ friends = {
   bob: %i(alice ed greg),
   ed: %i(hal),
   carl: %i(hal),
-  hal: %i(mark)
+  hal: %i(mark),
+  mark: %i(bob alice john),
+  john: %i(laura),
 }
 
 def min_jumps(u1, u2, friends, jumps = 0, blacklist = [])
-  # jumps ||= 0
-  # blacklist ||= []
   jumps_list ||= {}
 
   return jumps if u1 == u2
+
+  blacklist = blacklist.slice(0, 1) if jumps == 1 # Reset Blacklist when starting a new path
 
   jumps += 1
 
@@ -47,11 +51,24 @@ end
 
 puts min_jumps(:alice, :alice, friends)
 # => 0
+# path: Alice - Alice => 0
+
 puts min_jumps(:alice, :bob, friends)
 # => 1
+# path: Alice -> Bob => 1
+
 puts min_jumps(:alice, :ed, friends)
 # => 2
+# path: Alice -> Bob -> Ed => 2
+
 puts min_jumps(:alice, :hal, friends)
 # => 2
+# path: Alice -> Carl -> Hal => 2
+
 puts min_jumps(:alice, :mark, friends)
 # => 3
+# path: Alice -> Carl -> Hal -> Mark => 3
+
+puts min_jumps(:alice, :laura, friends)
+# => 5
+# path: Alice -> Carl -> Hal -> Mark -> John -> Laura => 5
